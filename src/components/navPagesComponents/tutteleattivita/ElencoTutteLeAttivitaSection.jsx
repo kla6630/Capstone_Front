fc179eb81de5c1924a528536b15b92ff3da00925
kla6630 <claudiopinna@live.com>
2023-06-06 23:02:17 +0200

    Update ElencoTutteLeAttivitaSection.jsx

--- src/components/navPagesComponents/tutteleattivita/ElencoTutteLeAttivitaSection.jsx
+++ src/components/navPagesComponents/tutteleattivita/ElencoTutteLeAttivitaSection.jsx
@@ -2,13 +2,18 @@ import { Container, Row, Col } from "react-bootstrap";
 import { ElencoTutteLeAttivitaCardFetch } from "./ElencoTutteLeAttivitaCardFetch";
 import { CreaNuovaAttivita } from "../../SingolaAttivita/nuovaAttivita/CreaNuovaAttivita";
 
+import { useContext } from "react";
+import AuthContext from "../../Auth/AuthContext";
+
 export const ElencoTutteLeAttivitaSection = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <Container className="py-5">
       <div className="d-flex">
         <h5>Tutte le Attivita</h5>
       </div>
-      <CreaNuovaAttivita />
+      {user && <CreaNuovaAttivita />}
       <Row className="mt-2">
         <Col>
           <ElencoTutteLeAttivitaCardFetch />
